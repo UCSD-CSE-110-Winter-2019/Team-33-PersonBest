31160e9252190e76a6f463f3a7585b02661dd454
reubenbouaziz <ax006237@acsmail.ucsd.edu>
2019-02-10 11:17:49 -0800

    last task easy-setup

--- app/src/main/java/com/android/personbest/MainActivity.java
+++ app/src/main/java/com/android/personbest/MainActivity.java
@@ -1,5 +1,7 @@
 package com.android.personbest;
 
+import android.content.ContentValues;
+import android.provider.BaseColumns;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,14 +11,26 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
     private EditText editText;
 
+    public static class DataBaseEntry implements BaseColumns {
+        public static final String DATABASE = "database";
+        public static final String DATA_TYPE = "data type";
+        public static final String VALUE = "value";
+    }
+    private static final String SQL_CREATE_ENTRIES = "CREATE TABLE " + DataBaseEntry.DATABASE + " (" + DataBaseEntry._ID + " INTEGER PRIMARY KEY," + DataBaseEntry.DATA_TYPE + " TEXT," + DataBaseEntry.VALUE + " TEXT)";
+    private static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + DataBaseEntry.DATABASE;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     public double calculateStrideLength(){
         editText = (EditText)findViewById(R.id.height);
         double height = Double.parseDouble(editText.getText().toString());
+        ContentValues values = new ContentValues();
+        values.put(DataBaseEntry.DATA_TYPE, "height");
+        values.put(DataBaseEntry.VALUE, Double.parseDouble(editText.getText().toString()));
         return 0.414*height;
     }
 }
