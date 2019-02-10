a1383ae3944b477ac5b0812eb482368a0ee13bb3
Jinghao Jia <jiajinghao1998@gmail.com>
2019-02-11 08:50:30 +0900

    Fixed coding style

--- app/src/main/java/com/android/personbest/IntentionalWalkUtils.java
+++ app/src/main/java/com/android/personbest/IntentionalWalkUtils.java
@@ -2,7 +2,7 @@ package com.android.personbest;
 
 public class IntentionalWalkUtils {
     public static final double STRIDE_CONST = 0.413; // Height in inch mult by 0.413 is the average stride length
-    public static final int feetOneMile = 5280; // Num of feet in one mile
+    public static final int FEET_ONE_MILE = 5280; // Num of feet in one mile
 
     // Returns Height from stride length
     public static double strideLength(double height) {
@@ -14,7 +14,7 @@ public class IntentionalWalkUtils {
         // When starting, velocity is 0
         if(timeInSeconds == 0)
             return 0;
-        double distanceInMile = strideLength(height) * numSteps / feetOneMile;
+        double distanceInMile = strideLength(height) * numSteps / FEET_ONE_MILE;
         return distanceInMile / (timeInSeconds / 3600.0);
     }
 }
