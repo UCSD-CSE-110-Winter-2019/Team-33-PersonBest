ab7c1ce52a4437c1a4ae5b1e982d601dd0b96542
reubenbouaziz <ax006237@acsmail.ucsd.edu>
2019-02-09 23:00:14 -0800

    calculate stride length done

--- app/src/main/java/com/android/personbest/MainActivity.java
+++ app/src/main/java/com/android/personbest/MainActivity.java
@@ -1,13 +1,22 @@
 package com.android.personbest;
 
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public double calculateStrideLength(){
+        editText = (EditText)findViewById(R.id.height);
+        double height = Double.parseDouble(editText.getText().toString());
+        return 0.414*height;
     }
 }
