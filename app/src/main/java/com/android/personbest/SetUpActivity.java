19b79e41a99079ba5239f5947ca3abcdf499efa3
reubenbouaziz <ax006237@acsmail.ucsd.edu>
2019-02-10 14:48:43 -0800

    edit last task

--- app/src/main/java/com/android/personbest/SetUpActivity.java
+++ app/src/main/java/com/android/personbest/SetUpActivity.java
@@ -9,14 +9,6 @@ import android.widget.EditText;
 public class SetUpActivity extends AppCompatActivity {
     private EditText editText;
 
-    public static class DataBaseEntry implements BaseColumns {
-        public static final String DATABASE = "database";
-        public static final String DATA_TYPE = "data type";
-        public static final String VALUE = "value";
-    }
-    private static final String SQL_CREATE_ENTRIES = "CREATE TABLE " + SetUpActivity.DataBaseEntry.DATABASE + " (" + SetUpActivity.DataBaseEntry._ID + " INTEGER PRIMARY KEY," + SetUpActivity.DataBaseEntry.DATA_TYPE + " TEXT," + SetUpActivity.DataBaseEntry.VALUE + " TEXT)";
-    private static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + SetUpActivity.DataBaseEntry.DATABASE;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,9 +18,6 @@ public class SetUpActivity extends AppCompatActivity {
     public double calculateStrideLength(){
         editText = (EditText)findViewById(R.id.height);
         double height = Double.parseDouble(editText.getText().toString());
-        ContentValues values = new ContentValues();
-        values.put(SetUpActivity.DataBaseEntry.DATA_TYPE, "height");
-        values.put(SetUpActivity.DataBaseEntry.VALUE, Double.parseDouble(editText.getText().toString()));
         return 0.414*height;
     }
 }
