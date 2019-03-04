e4c5082ebbce2af3574f96af75e152560e899f11
reubenbouaziz <ax006237@acsmail.ucsd.edu>
2019-03-03 16:03:40 -0800

    merge branch successfull

--- app/src/main/java/com/android/personbest/FriendListActivity.java
+++ app/src/main/java/com/android/personbest/FriendListActivity.java
@@ -2,7 +2,12 @@ package com.android.personbest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.android.personbest.FriendshipManager.Relations;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.firebase.FirebaseApp;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FriendListActivity extends AppCompatActivity {
 
@@ -11,5 +16,16 @@ public class FriendListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         FirebaseApp.initializeApp(this);
         setContentView(R.layout.activity_friend_list);
+    }
+
+    public List<String> getFriendList(){
+        Relations relations = new Relations(new MainActivity());
+        List<String> listEmail = relations.getFriends();
+        List<String> listFriendsName = new ArrayList<String>();
+        for (String email: listEmail) {
+            GoogleSignInAccount account = GoogleSignInAccount.
+            listFriendsName.add(account.);
+        }
+        return listFriendsName;
     }
 }
