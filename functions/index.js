994d95f9c436ca500ff3503798c19dd9b4737dbd
reubenbouaziz <ax006237@acsmail.ucsd.edu>
2019-03-10 23:48:20 -0700

    push notification

--- functions/index.js
+++ functions/index.js
@@ -13,3 +13,33 @@ exports.addTimeStamp = functions.firestore
 
      return "snap was null or empty";
    });
+exports.sendChatNotifications = functions.firestore
+   .document('chats/{chatId}/messages/{messageId}')
+   .onCreate((snap, context) => {
+     // Get an object with the current document value.
+     // If the document does not exist, it has been deleted.
+     const document = snap.exists ? snap.data() : null;
+
+     if (document) {
+       var message = {
+         notification: {
+           title: document.from + ' sent you a message',
+           body: document.text
+         },
+         topic: context.params.chatId
+       };
+
+       return admin.messaging().send(message)
+         .then((response) => {
+           // Response is a message ID string.
+           console.log('Successfully sent message:', response);
+           return response;
+         })
+         .catch((error) => {
+           console.log('Error sending message:', error);
+           return error;
+         });
+     }
+
+     return "document was null or emtpy";
+   });
