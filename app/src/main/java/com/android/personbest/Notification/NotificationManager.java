427aaeca2b8cb7058b7613179c90900c456e7915
Han Li <h5li@ucsd.edu>
2019-03-15 22:01:10 -0700

    Fix Progress Encouragement

--- app/src/main/java/com/android/personbest/Notification/NotificationManager.java
+++ app/src/main/java/com/android/personbest/Notification/NotificationManager.java
@@ -1,5 +1,6 @@
 package com.android.personbest.Notification;
 
+import android.app.NotificationChannel;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
@@ -15,6 +16,22 @@ public class NotificationManager implements INotification {
 
     public NotificationManager(MainActivity activity){
         this.activity = activity;
+        this.createNotificationChannel();
+    }
+
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        CharSequence name = "Goal Push Notification";
+        String description = "Check Goal";
+        int importance = 0;
+        NotificationChannel channel = new NotificationChannel("0", name, importance);
+        channel.setDescription(description);
+        // Register the channel with the system; you can't change the importance
+        // or other notification behaviors after this
+        NotificationManager notificationManager = this.activity.getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel();
     }
     @Override
     public void sendNotification(String msg) {
